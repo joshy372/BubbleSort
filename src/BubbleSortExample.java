01f4cef9f86fa2cbdae4f7183708499091d434ee
Janis Metzroth <janismetzroth@MacBookPro.fritz.box>
2025-03-18 11:00:25 +0100

    Tom: Umbenennung von arr zu numbers

--- src/BubbleSortExample.java
+++ src/BubbleSortExample.java
@@ -1,14 +1,14 @@
 public class BubbleSortExample {
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
+    static void bubbleSort(int[] numbers) {
+        int n = numbers.length;
         int temp = 0;
         for(int i=0; i < n; i++){
             for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
+                if(numbers[j-1] > numbers[j]){
                     //swap elements
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
+                    temp = numbers[j-1];
+                    numbers[j-1] = numbers[j];
+                    numbers[j] = temp;
                 }
             }
         }
